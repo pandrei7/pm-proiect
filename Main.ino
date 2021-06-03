3bd0029a9fc91811a72c0e40091372b3ab07e6a5
pandrei7 <preda.andrei174@gmail.com>
2021-06-03 10:25:28 +0300

    Document the code a bit

--- Main.ino
+++ Main.ino
@@ -3,23 +3,16 @@
 
 #include "./Songs.h"
 
+// Number of possible symbols.
 #define SYMBOLS 6
+// Number of symbols in a code sequence.
 #define SEQ_LEN 4
+// Number of attempts before the user loses.
 #define TRIES 5
+// Number of seconds before the user loses.
 #define TIME 100
 
-const int kButton = 8;
-const int kPot = A0;
-
-const int kLed0 = 7;
-const int kLed1 = 6;
-const int kLed2 = 5;
-const int kLed3 = 4;
-
-const int kBuzzer = 9;
-
-const LiquidCrystal_I2C kLcd(0x27, 16, 2);
-
+// A datatype for the state of the FSM.
 enum class State {
     kPregame,
     kPreInput,
@@ -29,25 +22,53 @@ enum class State {
     kGameOver,
 };
 
+// The current state of the FSM.
 State state = State::kPregame;
 
-FTDebouncer pin_debouncer;
+// User inputs.
+const FTDebouncer pin_debouncer;
+const int kButton = 8;
+const int kPot = A0;
 
+// Outputs for the user.
+const int kLed0 = 7;
+const int kLed1 = 6;
+const int kLed2 = 5;
+const int kLed3 = 4;
+const int kBuzzer = 9;
+const LiquidCrystal_I2C kLcd(0x27, 16, 2);
+
+// The secret code.
 char seq[SEQ_LEN + 1] = {};
+// The user's latest guess.
 char seq_tried[SEQ_LEN + 1] = {};
+// The number of symbols in the user's guess.
 int seq_tried_len = 0;
+
+// The number of attempts left.
 int tries = TRIES;
-bool button_is_pushed = false;
+// The number of seconds left before the game ends.
 int seconds_left = 0;
-bool game_started = false;
-bool player_won = false;
+
+// The number of correctly matched symbols in the last guess.
 int correct_elems = 0;
+// The number of partially matched symbols in the last guess.
 int partial_elems = 0;
 
+// The current state of the button (used for debouncing).
+bool button_is_pushed = false;
+
+// A flag set when the actual game starts.
+bool game_started = false;
+// A flag set when the player guesses correctly.
+bool player_won = false;
+
+// Called once per second once the game starts.
 ISR(TIMER1_COMPA_vect) {
     seconds_left -= 1;
 }
 
+// Seeds the random number generator with a fairly random value.
 static void seedRandomly() {
     // If `A3` is unconnected, the seed will be random analog noise.
     randomSeed(analogRead(A3));
@@ -62,12 +83,14 @@ static void seedRandomly() {
     }
 }
 
+// Chooses the secret code randomly.
 static void chooseRandomSeq() {
     for (int i = 0; i < SEQ_LEN; i += 1) {
         seq[i] = 'A' + random(0, SYMBOLS);
     }
 }
 
+// Converts the potentiometer reading into a symbol.
 static char convertToSymbol(int pot) {
     int interval_len = 1024 / SYMBOLS;
     int interval = pot / interval_len;
@@ -75,6 +98,8 @@ static char convertToSymbol(int pot) {
     return 'A' + interval;
 }
 
+// Checks if the button is pressed.
+// Only actual presses are counted, holding the button counts as one press.
 static bool buttonIsPressed() {
     auto res = button_is_pushed;
     if (button_is_pushed) {
@@ -83,6 +108,7 @@ static bool buttonIsPressed() {
     return res;
 }
 
+// Configures and starts the timer which ticks each second.
 static void startTimer() {
     cli();
     TCCR1A = 0;
@@ -96,6 +122,7 @@ static void startTimer() {
     sei();
 }
 
+// The FSM behaviour before the actual game starts.
 static void pregame() {
     if (buttonIsPressed()) {
         seconds_left = TIME;
@@ -105,6 +132,7 @@ static void pregame() {
     }
 }
 
+// The FSM behaviour before the player can make a guess.
 static void preInput() {
     kLcd.clear();
     kLcd.blink();
@@ -120,10 +148,11 @@ static void preInput() {
     state = State::kInput;
 }
 
+// The FSM behaviour while the user is inputting a guess.
 static void input() {
+    // Update the time counter, if needed.
     static int prev_seconds = 0;
     int curr_seconds = seconds_left;
-
     if (curr_seconds != prev_seconds) {
         kLcd.setCursor(10, 0);
         kLcd.print("T: ");
@@ -133,22 +162,26 @@ static void input() {
         kLcd.print(String(curr_seconds) + "s");
     }
 
+    // Preview the next symbol.
     char symbol = convertToSymbol(analogRead(kPot));
     int col = 2 + seq_tried_len;
     kLcd.setCursor(col, 1);
     kLcd.print(symbol);
     kLcd.setCursor(col, 1);
 
+    // Commit symbols when pressing the button.
     if (buttonIsPressed()) {
         seq_tried[seq_tried_len] = symbol;
         seq_tried_len += 1;
     }
+    // Once the whole guess is completed, try to get feedback.
     if (seq_tried_len == SEQ_LEN) {
         tries -= 1;
         state = State::kPreFeedback;
     }
 }
 
+// Computes the number of correct and partial matches of the current guess.
 static void evaluateGuess() {
     bool used[SEQ_LEN] = {};
 
@@ -172,6 +205,7 @@ static void evaluateGuess() {
     }
 }
 
+// Prints some debugging information to the serial interface.
 static void printDebugInfo() {
     Serial.println("Guessed: " + String(seq_tried));
     Serial.println(
@@ -179,6 +213,7 @@ static void printDebugInfo() {
     );
 }
 
+// The FSM behaviour before the user sees their feedback.
 static void preFeedback() {
     kLcd.clear();
     kLcd.noBlink();
@@ -187,13 +222,13 @@ static void preFeedback() {
     evaluateGuess();
     player_won = correct_elems >= SEQ_LEN;
 
-    // Light up LEDs for correct elements.
+    // Light up LEDs for correct matches.
     digitalWrite(kLed0, correct_elems > 0 ? HIGH : LOW);
     digitalWrite(kLed1, correct_elems > 1 ? HIGH : LOW);
     digitalWrite(kLed2, correct_elems > 2 ? HIGH : LOW);
     digitalWrite(kLed3, correct_elems > 3 ? HIGH : LOW);
 
-    // Display the number of partial elements.
+    // Display the number of partial matches.
     kLcd.setCursor(0, 0);
     kLcd.print("Partial: " + String(partial_elems));
 
@@ -204,20 +239,19 @@ static void preFeedback() {
     state = State::kFeedback;
 }
 
+// The FSM behaviour while the user is reading their feedback.
 static void feedback() {
     if (buttonIsPressed()) {
         state = State::kPreInput;
     }
 }
 
-static void preGameOver() {
-    state = State::kGameOver;
-}
-
+// Checks if the game should end.
 static bool gameIsOver() {
     return game_started && (player_won || tries <= 0 || seconds_left <= 0);
 }
 
+// The FSM behaviour after the game has ended.
 static void gameOver() {
     kLcd.clear();
     kLcd.noBlink();
@@ -235,6 +269,7 @@ static void gameOver() {
         kLcd.print("Game over!");
     }
 
+    // Print the solution.
     kLcd.setCursor(16 - SEQ_LEN, 0);
     kLcd.print(String(seq));
 
@@ -245,36 +280,41 @@ static void gameOver() {
     }
 }
 
+// A function required by `FTDebouncer`.
 void onPinActivated(int pin) {
     button_is_pushed = true;
 }
 
+// A function required by `FTDebouncer`.
 void onPinDeactivated(int pin) {
     button_is_pushed = false;
 }
 
 void setup() {
+    // User inputs.
     pinMode(kButton, INPUT_PULLUP);
     pinMode(kPot, INPUT);
+    pin_debouncer.addPin(kButton, HIGH, INPUT_PULLUP);
+    pin_debouncer.begin();
 
+    // Outputs.
     pinMode(kLed0, OUTPUT);
     pinMode(kLed1, OUTPUT);
     pinMode(kLed2, OUTPUT);
     pinMode(kLed3, OUTPUT);
-
     pinMode(kBuzzer, OUTPUT);
-
-    pin_debouncer.addPin(kButton, HIGH, INPUT_PULLUP);
-    pin_debouncer.begin();
 
     kLcd.init();
     kLcd.backlight();
+
+    // Display a welcome message.
     kLcd.clear();
     kLcd.setCursor(1, 0);
     kLcd.print("Press button");
     kLcd.setCursor(1, 1);
     kLcd.print("when ready...");
 
+    // Choose the secret code.
     seedRandomly();
     chooseRandomSeq();
 
--- Songs.h
+++ Songs.h
@@ -3,26 +3,33 @@
 
 #include "./Pitches.h"
 
+// This module contains functions and data types for interacting with songs.
+
+// The maximum number of notes in a song.
 #define MAX_SONG_LEN 100
 
+// All attributes of a song.
 struct Song {
     int melody[MAX_SONG_LEN];
     int durations[MAX_SONG_LEN];
     int len;
 };
 
+// A song to celebrate winning the game.
 const Song kHappySong = {
     { NOTE_C4, NOTE_G3, NOTE_G3, NOTE_A3, NOTE_G3, 0, NOTE_B3, NOTE_C4 },
     { 4, 8, 8, 4, 4, 4, 4, 4 },
     8
 };
 
+// A song used when the player loses the game.
 const Song kSadSong = {
     { NOTE_C4, NOTE_G3, NOTE_G3, NOTE_A3, NOTE_G3, 0, NOTE_B3, NOTE_C4 },
     { 4, 8, 8, 4, 4, 4, 4, 4 },
     8
 };
 
+// Plays a song on a given buzzer pin.
 void playSong(int buzzer, const Song& song);
 
 #endif
